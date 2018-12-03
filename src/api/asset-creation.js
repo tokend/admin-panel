920077572fe374c15c285d07a5662e2abfd96a03
Roman Malyshev <lem0000000000n@gmail.com>
2018-12-03 16:47:22 +0200

    add usage new-sdk in asset-creation

--- src/api/asset-creation.js
+++ src/api/asset-creation.js
@@ -1,44 +1,29 @@
-import { ManageAssetBuilder } from 'tokend-js-sdk'
-import { ServerCallBuilder } from './ServerCallBuilder'
-import { envelopOperations } from './helpers/envelopOperations'
-
-const ScopedServerCallBuilder = ServerCallBuilder.makeScope()
-  .registerResource('transactions')
+import { Sdk } from '@/sdk'
 
 export const assetCreation = {
   createAsset (params) {
-    const tx = envelopOperations(
-      ManageAssetBuilder.assetCreationRequest({
-        requestID: '0',
-        code: String(params.code),
-        preissuedAssetSigner: String(params.preissued_asset_signer || params.preissuedAssetSigner),
-        maxIssuanceAmount: String(params.max_issuance_amount || params.maxIssuanceAmount),
-        policies: Number(params.policy),
-        initialPreissuedAmount: params.initialPreissuedAmount,
-        details: params.details
-      })
-    )
+    const operation = Sdk.base.ManageAssetBuilder.assetCreationRequest({
+      requestID: '0',
+      code: String(params.code),
+      preissuedAssetSigner: String(params.preissued_asset_signer || params.preissuedAssetSigner),
+      maxIssuanceAmount: String(params.max_issuance_amount || params.maxIssuanceAmount),
+      policies: Number(params.policy),
+      initialPreissuedAmount: params.initialPreissuedAmount,
+      details: params.details
+    })
 
-    return new ScopedServerCallBuilder()
-      .transactions()
-      .sign()
-      .post({ tx })
+    return Sdk.horizon.transactions.submitOperations(operation)
   },
 
   updateAsset (params) {
-    const tx = envelopOperations(
-      ManageAssetBuilder.assetUpdateRequest({
-        requestID: '0',
-        code: String(params.code),
-        policies: Number(params.policy),
-        logoId: String(params.logo_id || params.logoId),
-        details: params.details
-      })
-    )
+    const operation = Sdk.base.ManageAssetBuilder.assetUpdateRequest({
+      requestID: '0',
+      code: String(params.code),
+      policies: Number(params.policy),
+      logoId: String(params.logo_id || params.logoId),
+      details: params.details
+    })
 
-    return new ScopedServerCallBuilder()
-      .transactions()
-      .sign()
-      .post({ tx })
+    return Sdk.horizon.transactions.submitOperations(operation)
   }
 }
