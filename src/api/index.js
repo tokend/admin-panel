663ef5da7aa57eca0329854fabfd2afbadef8a23
Roman Malyshev <lem0000000000n@gmail.com>
2018-12-10 18:44:03 +0200

    Remove unused forfeits

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,6 @@
 import accounts from './accounts'
 import assets from './assets'
 import documents from './documents'
-import forfeits from './forfeits'
 import keyValue from './key-value'
 import limits from './limits'
 import tfa from './tfa'
@@ -20,7 +19,6 @@ export default {
   assets: assets,
   accounts: accounts,
   documents: documents,
-  forfeits: forfeits,
   keyValue: keyValue,
   limits: limits,
   tfa: tfa,
