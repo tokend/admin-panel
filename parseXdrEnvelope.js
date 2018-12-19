06f2568e7611033cb991b953035983968b8afefb
Roman Malyshev <lem0000000000n@gmail.com>
2018-12-19 16:18:44 +0200

    Fix import in parseXdrEnvelope.js

--- parseXdrEnvelope.js
+++ parseXdrEnvelope.js
@@ -1,11 +1,12 @@
-import { Sdk } from '@/sdk'
+const Sdk = require('@tokend/js-sdk').base
 
 function parseEnvelope (envelope) {
+  console.log('parseEnvelopeparseEnvelopeparseEnvelopeparseEnvelope')
   const buffer = new Buffer(envelope, 'base64')
   const transaction = Sdk.xdr.TransactionEnvelope.fromXDR(buffer)
   const operations = transaction.tx().operations()
-  if (operations.length === 1) return Sdk.base.Operation.operationToObject(operations[0])
-  return operations.map(operation => Sdk.base.Operation.operationToObject(operation)) // .operationToObject(operations)
+  if (operations.length === 1) return Sdk.Operation.operationToObject(operations[0])
+  return operations.map(operation => Sdk.Operation.operationToObject(operation)) // .operationToObject(operations)
 }
 
 console.log(parseEnvelope(global.process.argv[2]))
