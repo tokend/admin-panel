0e2b45464e17269cfc7b0bbb4994e9fdc45ff599
Maksym Nesterov <maxym.nes.dev@gmail.com>
2019-01-24 21:10:00 +0200

    Refactor uploadFile build

--- src/api/documents.js
+++ src/api/documents.js
@@ -3,25 +3,17 @@ import Vue from 'vue'
 export default {
   uploadFile (file, config, mimeString) {
     const formData = new FormData()
-    const url = config.url
-    delete config.url
-    delete config.id
-    delete config.type
-    config['x-amz-algorithm'] = config.xAmzAlgorithm
-    config['x-amz-credential'] = config.xAmzCredential
-    config['x-amz-date'] = config.xAmzDate
-    config['x-amz-signature'] = config.xAmzSignature
+    formData.append('bucket', config.formData.bucket)
+    formData.append('key', config.formData.key)
+    formData.append('policy', config.formData.policy)
+    formData.append('x-amz-algorithm', config.formData.x_amz_algorithm)
+    formData.append('x-amz-credential', config.formData.x_amz_credential)
+    formData.append('x-amz-date', config.formData.x_amz_date)
+    formData.append('x-amz-signature', config.formData.x_amz_signature)
 
-    delete config.xAmzAlgorithm
-    delete config.xAmzCredential
-    delete config.xAmzDate
-    delete config.xAmzSignature
-
-    for (const key in config) {
-      formData.append(key, config[key])
-    }
     const blob = new Blob([file], { type: mimeString })
     formData.append('file', blob)
-    return Vue.http.post(url, formData)
+
+    return Vue.http.post(config.url, formData)
   }
 }
