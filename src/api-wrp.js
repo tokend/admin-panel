d034768886b560687eb39b33921c21121ef13735
Maksym Nesterov <maxym.nes.dev@gmail.com>
2019-02-21 15:37:27 +0200

    Add config getter to ApiWrp

--- src/api-wrp.js
+++ src/api-wrp.js
@@ -26,11 +26,23 @@ export class ApiWrp {
   }
 
   /**
-  * Sets default network passphrase to be used on creating new caller instances
-  * @param {string} horizonUrl
-  */
+   * Sets default network passphrase to be used on creating new caller instances
+   * @param {string} horizonUrl
+   */
   static setDefaultNetworkPassphrase (networkPassphrase) {
     defaultNetworkPassphrase = networkPassphrase
+  }
+
+  static get defaultHorizonUrl () {
+    return defaultHorizonUrl
+  }
+
+  static get defaultWallet () {
+    return defaultWallet
+  }
+
+  static get defaultNetworkPassphrase () {
+    return defaultNetworkPassphrase
   }
 
   /**
@@ -41,25 +53,25 @@ export class ApiWrp {
    * @param {Object} [opts.networkPassphrase]
    */
   static createCallerInstance (opts = {}) {
-    if (!opts.horizonUrl && !defaultHorizonUrl) {
-      throw new Error('ApiWrp.createCallerInstance(): neither opts.horizonUrl nor defaultHorizonUrl provided')
+    if (!opts.horizonUrl && !ApiWrp.defaultHorizonUrl) {
+      throw new Error('ApiWrp.createCallerInstance(): neither opts.horizonUrl nor ApiWrp.defaultHorizonUrl provided')
     }
 
-    const horizonUrl = opts.horizonUrl || defaultHorizonUrl
+    const horizonUrl = opts.horizonUrl || ApiWrp.defaultHorizonUrl
     const callerInstance = ApiCaller.getInstance(horizonUrl)
 
-    if (!opts.wallet && !defaultWallet) {
-      throw new Error('ApiWrp.createCallerInstance(): neither opts.wallet nor defaultWallet provided')
+    if (!opts.wallet && !ApiWrp.defaultWallet) {
+      throw new Error('ApiWrp.createCallerInstance(): neither opts.wallet nor ApiWrp.defaultWallet provided')
     }
 
-    const wallet = opts.wallet || defaultWallet
+    const wallet = opts.wallet || ApiWrp.defaultWallet
     callerInstance.useWallet(wallet)
 
-    if (!opts.networkPassphrase && !defaultNetworkPassphrase) {
-      throw new Error('ApiWrp.createCallerInstance(): neither opts.networkPassphrase nor defaultNetworkPassphrase provided')
+    if (!opts.networkPassphrase && !ApiWrp.defaultNetworkPassphrase) {
+      throw new Error('ApiWrp.createCallerInstance(): neither opts.networkPassphrase nor ApiWrp.defaultNetworkPassphrase provided')
     }
 
-    const networkPassphrase = opts.networkPassphrase || defaultNetworkPassphrase
+    const networkPassphrase = opts.networkPassphrase || ApiWrp.defaultNetworkPassphrase
     callerInstance.usePassphrase(networkPassphrase)
 
     return callerInstance
