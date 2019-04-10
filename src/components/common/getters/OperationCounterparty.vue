9cfc1faed305c24a13cfaa5e0a07f1f91106ed58
Unknown <datik94@gmail.com>
2019-04-10 18:36:22 +0300

    Edit OperationCounterparty getter

--- src/components/common/getters/OperationCounterparty.vue
+++ src/components/common/getters/OperationCounterparty.vue
@@ -4,50 +4,15 @@
   </div>
 </template>
 <script>
-  import { OP_TYPES } from '@/constants'
-  import get from 'lodash/get'
-
   export default {
     props: ['operation'],
     data: _ => ({
       counterparty: 'counterparty',
-      noCounterpartyStr: '-',
-      manyCounterpartyStr: 'many'
+      noCounterpartyStr: '-'
     }),
     methods: {
       getCounterparty (operation) {
-        switch (operation.typeI) {
-          case OP_TYPES.createIssuanceRequest:
-            return operation.participants.find(p => !p.balanceId).accountId
-          case OP_TYPES.manageOffer:
-            if (operation.orderBookId && operation.orderBookId !== '0') {
-              return this.noCounterpartyStr
-            }
-            const counterparties = operation.participants
-            .filter(p => p.accountId !== operation.sourceAccount)
-            if (counterparties.length > 1) {
-              return this.manyCounterpartyStr
-            } else {
-              return get(counterparties[0], 'accountId')
-            }
-          case OP_TYPES.checkSaleState:
-            return operation.baseAsset
-          case OP_TYPES.payment:
-            return operation.from
-          case OP_TYPES.paymentV2:
-            return operation.from
-          case OP_TYPES.createWithdrawalRequest:
-            return operation.creatorDetails.address
-          case OP_TYPES.reviewRequest:
-            if (operation.sourceAccount === 'Master') {
-              return this.noCounterpartyStr
-            }
-            return operation.participants.find(p => {
-              return p.accountId !== operation.sourceAccount
-            }).accountId
-          default:
-            return this.noCounterpartyStr
-        }
+        return operation.receiverAccount || this.noCounterpartyStr
       }
     },
     created () {
