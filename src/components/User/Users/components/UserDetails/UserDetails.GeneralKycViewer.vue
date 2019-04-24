d2a7565a53a23a2a7aaeddeb1358cfa8f5867bd1
Unknown <datik94@gmail.com>
2019-04-24 18:44:22 +0300

    Edit template to show new user roles

---
+++ src/components/User/Users/components/UserDetails/UserDetails.GeneralKycViewer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="user-details-general-kyc-viewer">
+    <kyc-general-section
+      :kyc="kyc"
+      :user="user"
+      :isKycLoaded="isKycLoaded"
+      :isKycLoadFailed ="isKycLoadFailed"
+    />
+  </div>
+</template>
+
+<script>
+import KycGeneralSection from './UserDetails.Kyc'
+export default {
+  components: {
+    KycGeneralSection
+  },
+  props: ['kyc', 'user', 'isKycLoaded', 'isKycLoadFailed']
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../../../../assets/scss/_colors.scss";
+ .user-details-general-kyc-viewer__header {
+   color: $color-info;
+ }
+</style>
---
+++ src/components/User/Users/components/UserDetails/UserDetails.VerifiedKycViewer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="user-details-verified-kyc-viewer">
+    <kyc-general-section
+      :kyc="kyc"
+      :user="user"
+      :isKycLoaded="isKycLoaded"
+      :isKycLoadFailed ="isKycLoadFailed" />
+    />
+  </div>
+</template>
+
+<script>
+import KycGeneralSection from './UserDetails.Kyc'
+export default {
+  components: {
+    KycGeneralSection
+  },
+  props: ['kyc', 'user', 'isKycLoaded', 'isKycLoadFailed']
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
