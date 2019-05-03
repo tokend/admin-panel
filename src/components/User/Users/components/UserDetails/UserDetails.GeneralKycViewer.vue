d06e1b27ad2983e671b73e3d8e7c251abe711515
Aleksandr <aleksandrplotnikov1997@gmail.com>
2019-05-03 16:21:28 +0300

    merge master

---
+++ src/components/User/Users/components/UserDetails/UserDetails.GeneralKycViewer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="user-details-general-kyc-viewer">
+    <kyc-general-section
+      :kyc="kyc"
+      :user="user"
+    />
+  </div>
+</template>
+
+<script>
+import KycGeneralSection from './UserDetails.Kyc'
+
+export default {
+  components: {
+    KycGeneralSection,
+  },
+  props: {
+    kyc: {
+      type: Object,
+      required: true,
+    },
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
