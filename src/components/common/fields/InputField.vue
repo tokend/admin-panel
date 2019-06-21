a368af014f98e1d37d6dd6e30a00d3eb1110b845
Maksym Nesterov <maxym.nes.dev@gmail.com>
2019-06-21 18:15:15 +0300

    Comparre with math.util in Input field, do not enforce min

--- src/components/common/fields/InputField.vue
+++ src/components/common/fields/InputField.vue
@@ -42,7 +42,10 @@
     </div>
 
     <transition name="input-field__err-transition">
-      <p class="input-field__err-mes" v-if="errorMessage">
+      <p
+        class="input-field__err-mes"
+        v-if="errorMessage"
+      >
         {{ errorMessage }}
       </p>
     </transition>
@@ -51,6 +54,7 @@
 
 <script>
 import InputFieldAutocomplete from './InputFieldAutocomplete'
+import { MathUtil } from '@/utils/math.util'
 
 const EVENTS = {
   input: 'input',
@@ -86,12 +90,12 @@ export default {
   },
 
   methods: {
-    onInput (event) {},
+    onInput (event) { },
 
     beforeEmit (target) {
       if (this.type === 'number') {
         this.normalizeNumberFormat(target)
-        this.normalizeMinMax(target)
+        this.normalizeMax(target)
         this.normalizeDecimalPrecision(target)
       }
     },
@@ -105,17 +109,16 @@ export default {
       }
     },
 
-    normalizeMinMax (target) {
+    normalizeMax (target) {
       const { value } = target
-      const max = '' + this.$attrs.max
-      const min = '' + this.$attrs.min
 
-      if (value === '') {
+      if (!value) {
+        return
+      }
 
-      } else if (value > +max) {
+      const max = this.$attrs.max
+      if (MathUtil.compare(value, max) > 0) {
         target.value = max
-      } else if (value < +min) {
-        target.value = min
       }
     },
 
@@ -144,5 +147,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import "./scss/input";
+@import './scss/input';
 </style>
