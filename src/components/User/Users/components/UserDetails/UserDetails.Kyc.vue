7838e2d3b9862cdfe06d222e2ba429f303dcbf4f
Rolaxxx3 <ruslannightcore@outlook.com>
2019-12-30 20:36:32 +0200

    add localization UserDetails.Kyc

--- src/components/User/Users/components/UserDetails/UserDetails.Kyc.vue
+++ src/components/User/Users/components/UserDetails/UserDetails.Kyc.vue
@@ -1,44 +1,38 @@
 <template>
   <div class="user-details-account">
     <h3 class="user-details-account__title">
-      KYC information
+      {{ "user-details-kyc.header" | globalize }}
     </h3>
     <div class="user-details-account__info">
-      <p>Can be uploaded one of the documents:</p>
-      <p> - Valid Passport;</p>
-      <p> - Valid Identity card;</p>
-      <p> - Valid Residence permit;</p>
-      <p>
-        - Driving license - full, not provisional, with a color photograph;
-      </p>
+      {{ "user-details-kyc.uploaded-document" | globalize }}
     </div>
     <ul class="user-details-account__key-value-list key-value-list">
       <li>
-        <span>First name</span>
+        <span>{{ "user-details-kyc.first-name" | globalize }}</span>
         <span :title="kyc.firstName">
           {{ kyc.firstName }}
         </span>
       </li>
       <li>
-        <span>Last name</span>
+        <span>{{ "user-details-kyc.last-name" | globalize }}e</span>
         <span :title="kyc.lastName">
           {{ kyc.lastName }}
         </span>
       </li>
       <li>
-        <span>Date of birth</span>
+        <span>{{ "user-details-kyc.date-birth" | globalize }}</span>
         <span :title="kyc.dateOfBirth">
           {{ kyc.dateOfBirth | dateTime }}
         </span>
       </li>
       <li>
-        <span>Address Line 1</span>
+        <span>{{ "user-details-kyc.address-line1" | globalize }}</span>
         <span :title="kyc.address.line1">
           {{ kyc.address.line1 }}
         </span>
       </li>
       <li>
-        <span>Address Line 2</span>
+        <span>{{ "user-details-kyc.address-line2" | globalize }}</span>
         <span :title="kyc.address.line2">
           <template v-if="kyc.address.line2">
             {{ kyc.address.line2 }}
@@ -49,75 +43,77 @@
         </span>
       </li>
       <li>
-        <span>Postal code</span>
+        <span>{{ "user-details-kyc.postal-code" | globalize }}</span>
         <span :title="kyc.address.postalCode">
           {{ kyc.address.postalCode }}
         </span>
       </li>
       <li>
-        <span>City</span>
+        <span>{{ "user-details-kyc.city" | globalize }}</span>
         <span :title="kyc.address.city">
           {{ kyc.address.city }}
         </span>
       </li>
       <li>
-        <span>Country</span>
+        <span>{{ "user-details-kyc.country" | globalize }}</span>
         <span :title="country">
           {{ country }}
         </span>
       </li>
       <li>
-        <span>State</span>
+        <span>{{ "user-details-kyc.state" | globalize }}</span>
         <span :title="kyc.address.state">
           {{ kyc.address.state }}
         </span>
       </li>
       <li>
-        <span>ID Document type</span>
+        <span>{{ "user-details-kyc.id-document-type" | globalize }}</span>
         <span :title="ID_DOCUMENTS_VERBOSE[kyc.idDocumentType]">
           {{ ID_DOCUMENTS_VERBOSE[kyc.idDocumentType] }}
         </span>
       </li>
       <li>
         <span v-if="kyc.documents.kycIdDocument.back">
-          ID Document front side:
+          {{ "user-details-kyc.id-document-front-size" | globalize }}
         </span>
         <span v-else>
-          ID Document
+          {{ "user-details-kyc.id-document" | globalize }}
         </span>
         <span>
           <user-doc-link-getter
             :file-key="kyc.documents.kycIdDocument.face"
           >
-            Open file
+            {{ "user-details-kyc.open-file" | globalize }}
           </user-doc-link-getter>
         </span>
       </li>
       <li v-if="kyc.documents.kycIdDocument.back">
-        <span>ID Document back side: </span>
+        <span>{{ "user-details-kyc.id-document-back-size" | globalize }} </span>
         <span>
           <user-doc-link-getter
             :file-key="kyc.documents.kycIdDocument.back"
           >
-            Open file
+            {{ "user-details-kyc.open-file" | globalize }}
           </user-doc-link-getter>
         </span>
       </li>
       <li v-if="kyc.documents.kycAvatar">
-        <span>Avatar</span>
+        <span>{{ "user-details-kyc.avatar" | globalize }}</span>
         <span>
           <user-doc-link-getter :file-key="kyc.documents.kycAvatar">
-            Open file
+            {{ "user-details-kyc.open-file" | globalize }}
           </user-doc-link-getter>
         </span>
       </li>
       <li v-if="kyc.documents.kycSelfie">
-        <span>Photo with verification code</span>
+        <span>
+          {{ "user-details-kyc.photo-with-verification-code" | globalize }}
+        </span>
         <span>
           <user-doc-link-getter
             :file-key="kyc.documents.kycSelfie"
           >
-            Open file
+            {{ "user-details-kyc.open-file" | globalize }}
           </user-doc-link-getter>
         </span>
       </li>
@@ -125,7 +121,7 @@
         class="code-details"
         v-if="kyc.documents.kycSelfie"
       >
-        Code: {{ user.address.slice(1, 6) }}
+        {{ "user-details-kyc.code" | globalize }}
       </li>
     </ul>
   </div>
