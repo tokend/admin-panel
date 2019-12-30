9f275db17630ea00d2c02896ac6fa42a3c2fc295
Rolaxxx3 <ruslannightcore@outlook.com>
2019-12-30 18:29:55 +0200

    add localization TradeFilters

--- src/components/User/Trades/components/TradeFilters.vue
+++ src/components/User/Trades/components/TradeFilters.vue
@@ -16,19 +16,19 @@
 
         <template v-else-if="isLoaded && !pairs.length">
           <option value="" disabled>
-            No pairs yet
+            {{ "trade-filters.no-pairs" | globalize }}
           </option>
         </template>
 
         <template v-else-if="isFailed">
           <option value="" disabled>
-            An error occurred
+            {{ "trade-filters.error" | globalize }}
           </option>
         </template>
 
         <template v-else>
           <option value="" disabled>
-            Loading...
+            {{ "trade-filters.loading" | globalize }}
           </option>
         </template>
       </select-field>
